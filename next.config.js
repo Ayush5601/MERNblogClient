3f8d3c9a53721ab342609198f71bbde7e78b96e8
Ayush Agrawal <ayush123ngp@gmail.com>
2021-04-05 23:21:04 +0530

    updated

---
+++ next.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+    publicRuntimeConfig: {
+        APP_NAME: "Ayush's Blog",
+        API_DEVELOPMENT: 'http://localhost:8000/api',
+        API_PRODUCTION: 'https://ayushblogapp.herokuapp.com/api', 
+        PRODUCTION: true,
+        DOMAIN_DEVELOPMENT:'http://localhost:3000',
+        DOMAIN_PRODUCTION: 'https://seoblog.com',
+        DISQUS_SHORTNAME:'seoblog-uwlmdtnekv',
+        GOOGLE_CLIENT_ID:'1067493142377-qd0cv6nm2fg9qeddfsgdsgdf91b0ll5u8emi14dis.apps.googleusercontent.com'
+    }
+};
