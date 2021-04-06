0c8001040937822c4398aedd2c1d8648436bd4c0
Ayush Agrawal <ayush123ngp@gmail.com>
2021-04-06 19:34:29 +0530

    added

--- next.config.js
+++ next.config.js
@@ -7,6 +7,6 @@ module.exports = {
         DOMAIN_DEVELOPMENT:'http://localhost:3000',
         DOMAIN_PRODUCTION: 'https://mern-blog-client.vercel.app',
         DISQUS_SHORTNAME:'seoblog-uwlmdtnekv',
-        GOOGLE_CLIENT_ID:'1067493142377-qd0cv6nm2fg9qeddfsgdsgdf91b0ll5u8emi14dis.apps.googleusercontent.com'
+        GOOGLE_CLIENT_ID:'747227885974-5htgjp9phc6ojei0ptd5bngsjc5km2go.apps.googleusercontent.com'
     }
 };
