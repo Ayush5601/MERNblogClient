c6f2869766aff856b2294510eaea9db17a1d2677
Ayush Agrawal <ayush123ngp@gmail.com>
2021-04-06 18:36:59 +0530

    added

--- next.config.js
+++ next.config.js
@@ -5,7 +5,7 @@ module.exports = {
         API_PRODUCTION: 'https://ayushblogapp.herokuapp.com/api', 
         PRODUCTION: true,
         DOMAIN_DEVELOPMENT:'http://localhost:3000',
-        DOMAIN_PRODUCTION: 'https://ayushblogapp.netlify.app',
+        DOMAIN_PRODUCTION: 'https://mern-blog-client.vercel.app',
         DISQUS_SHORTNAME:'seoblog-uwlmdtnekv',
         GOOGLE_CLIENT_ID:'1067493142377-qd0cv6nm2fg9qeddfsgdsgdf91b0ll5u8emi14dis.apps.googleusercontent.com'
     }
